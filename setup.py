bb159316d072c13fe6cb9bf8277b0bbc71f8fc98
Amul Thantharate <89029777+Amul-Thantharate@users.noreply.github.com>
2024-11-23 12:41:14 +0530

    chore: bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except FileNotFoundError:
 
 setup(
     name='omenicli',
-    version='0.1',
+    version='0.1.1',
     author='Amul Thantharate',
     author_email='amulthantharate@gmail.com',
     license='MIT',
